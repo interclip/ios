da5bd63fce01290e3f127ba8c1a29cbc3046ea78
Filip Troníček <filip@gitpod.io>
2024-06-12 21:20:55 +0200

    Initial Commit

---
+++ Interclip/InterclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterclipApp.swift
+//  Interclip
+//
+//  Created by Filip Troníček on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct InterclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterclipTests/InterclipTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterclipTests.swift
+//  InterclipTests
+//
+//  Created by Filip Troníček on 12.06.2024.
+//
+
+import Testing
+@testable import Interclip
+
+struct InterclipTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
